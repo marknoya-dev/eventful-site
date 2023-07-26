3d0756fbb3277239562e387edcaceb4465da81b8
marknoya-eventful <marknoya@eventful.ph>
2023-07-26 18:20:58 +0800

    Fix attempt #3 on Floor Plan path

--- src/app/events/(users)/ickhoy-joy-wedding/findtable/[id]/page.tsx
+++ src/app/events/(users)/ickhoy-joy-wedding/findtable/[id]/page.tsx
@@ -5,7 +5,6 @@ import { Button } from "@eventful-ph/stark";
 import { useRouter } from "next/navigation";
 import { motion, useAnimate, stagger } from "framer-motion";
 import Image from "next/image";
-import path from "path";
 
 export default function Page({ params }: { params: { id: string } }) {
   const router = useRouter();
@@ -28,9 +27,7 @@ export default function Page({ params }: { params: { id: string } }) {
         { delay: stagger(0.1, { startDelay: 0.15 }), ease: "easeInOut" }
       );
 
-      setFloorPlan(
-        `/../public/floorplan/Table${" "}${tableData.guest.table}.jpg`
-      );
+      setFloorPlan(`/floorplan/Table${" "}${tableData.guest.table}.jpg`);
     }
   }, [animate, tableData, router]);
 
